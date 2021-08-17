9d3f9bb1e5a585e66a37359b3757496366d6b3a8
Melvyn Sopacua <78786958+melvyn-oviavo@users.noreply.github.com>
2021-08-17 17:55:40 +0200

    feat(FR-819): Login, logout and variables
    Align variables with core Django admin for most things and try to work
    out from login/logout page where they are used and why.

---
+++ material/static/material/fonts/rename-font.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import base64
+import hashlib
+import os
+import sys
+
+
+def main(*filenames: str):
+    for filename in filenames:
+        with open(filename, 'rb') as f:
+            h = hashlib.sha3_384(f.read())
+        new_name = base64.urlsafe_b64encode(h.digest()).decode('utf-8')
+        print(f"{filename} -> {new_name}.woff2")
+        os.rename(filename, f"{new_name}.woff2")
+
+
+if __name__ == '__main__':
+    main(*sys.argv[1:])
