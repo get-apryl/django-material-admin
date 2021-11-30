7c9d24dec99308acf4894cc015bf495d57adbb25
Melvyn Sopacua <78786958+melvyn-oviavo@users.noreply.github.com>
2021-11-30 12:06:33 +0100

    chore(FR-822): Bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-material-admin",
-    version="2021.11.23",
+    version="2021.11.30",
     license='MIT License',
     packages=find_packages(),
     author=["Anton Maistrenko", "Melvyn Sopacua"],
