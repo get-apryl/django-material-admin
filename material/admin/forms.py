95089b20b2ae4e2131fa95241ffd5914de2e9a1e
Melvyn Sopacua <78786958+melvyn-apryl@users.noreply.github.com>
2022-05-12 09:43:52 +0200

    feat(timezone): Add timezone support
    - In the login form add a hidden field with the resolved timezone
    - Store it in the session, if it has been resolved

---
+++ material/admin/forms.py
@@ -0,0 +1,20 @@
+from django.contrib.admin.forms import AdminAuthenticationForm
+from django.forms import HiddenInput, CharField
+import pytz
+from django.utils import timezone
+
+class BackofficeAuthenticationForm(AdminAuthenticationForm):
+    timezone = CharField(widget=HiddenInput, max_length=64, required=False)
+
+    def clean_timezone(self):
+        tz = self.data['timezone']
+        try:
+            cleaned = pytz.timezone(tz)
+        except pytz.exceptions.UnknownTimeZoneError:
+            cleaned = None
+        else:
+            timezone.activate(cleaned)
+            self.request.session['django_timezone'] = str(cleaned)
+
+        return cleaned
+
